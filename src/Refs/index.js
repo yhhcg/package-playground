0112192be4d7d0db1ad201a756dacaf8c6d02138
余珩 <1281005689@qq.com>
2019-05-26 21:52:35 +0800

    [Refs] Contrast the refs created by react.createRef method and the ref received through function

--- src/Refs/index.js
+++ src/Refs/index.js
@@ -1,23 +1,53 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'antd';
 
+/**
+ * Contrast the refs created by react.createRef method and the ref received through function.
+ */
 class Refs extends Component {
   constructor(props) {
     super(props);
 
     this.inputElement = React.createRef();
-    console.log('create');
+    this.buttonElement = React.createRef();
+    console.group();
+    console.log('React.createRef and initial');
     console.log(this.inputElement);
+    console.log(this.buttonElement);
+    console.groupEnd();
   }
 
   componentDidMount() {
-    console.log('didMount');
+    console.group();
+    console.log('React.createRef and HTML DOM ref');
     console.log(this.inputElement);
+    console.log('React.createRef and Class component ref');
+    console.log(this.buttonElement);
+    console.groupEnd();
   }
 
   render() {
     return (
-      <input ref={this.inputElement} />
+      <React.Fragment>
+        <h1>React.createRef</h1>
+        <p>Accessible at the current attribute of the ref.</p>
+        <input ref={this.inputElement} />
+        <br />
+        <Button ref={this.buttonElement}>button</Button>
+        <h1>callback</h1>
+        <p>The function receives the React component instance or HTML DOM element as its argument.</p>
+        <input ref={(ref) => {
+          console.log('callback and html dom ref');
+          console.log(ref);
+        }} />
+        <br />
+        <Button ref={(ref) => {
+          console.log('callback and class component ref');
+          console.log(ref);
+        }}>button</Button>
+      </React.Fragment>
+      
     )
   }
 }
